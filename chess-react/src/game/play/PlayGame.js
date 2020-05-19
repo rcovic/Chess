2871ab2a37fde761f7e0322b3fc6f1d1b128057f
rcovic <rcovic7@outlook.it>
2020-05-19 16:24:38 +0200

    Update PlayGame.js

--- chess-react/src/game/play/PlayGame.js
+++ chess-react/src/game/play/PlayGame.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import WithMoveValidation from './WithMoveValidation'
+
+
 
 export default function PlayGame() {
-
 
     // after every move check the request , if 
     // not my game (unauthorized) then redirect to home
 
     return (
         <div className="container">
-            
+          <WithMoveValidation />
         </div>
     );
 }
